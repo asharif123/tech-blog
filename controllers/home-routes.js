0ef698cc274927b0788a6248bb4f9aa16ef16169
Awad Sharif <awadsharif9@gmail.com>
2022-01-17 12:38:08 -0800

    defined routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+// defin login route
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login')
+})
+
+module.exports = router;
